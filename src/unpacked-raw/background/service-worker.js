cf23c8354fdf132238b4101508abbca4b3d56dd8
Loic Devaux <devloic@gmail.com>
2023-06-19 08:15:24 -0400

    removed unrelated code in background.js

--- src/unpacked-raw/background/service-worker.js
+++ src/unpacked-raw/background/service-worker.js
@@ -1,45 +1,9 @@
 console.log('background.js running');
 async function postData(url, options) {
-  // Default options are marked with *
-  //method: "GET"
-  var method = "POST";
-  var response;
-  if (options.method=="GET"){
-     response = await fetch(url);
-  }
- if (options.method=="POST"){
  
-  response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: options.headers,
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: options.body, // body data type must match "Content-Type" header
-  });
-}
- if (options.method=="PUT"){
-  response = await fetch(url, {
-    method: "PUT", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: options.headers,
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: options.body, // body data type must match "Content-Type" header
-  });
-}
-
 
   var response2 = {};
-  response2.data=await response.json();
-  //todo not the right logic, reverse origina extension
-  response2.ok=response.ok
-  //response2.data.token=response2.data.auth_token;
-  response2.status=response.status;
+  
 
   return response2; // parses JSON response into native JavaScript objects
 }
